1ddce00c3f2d345a7f23f77dd195196367ff0853
a-andrievskikh <sn0abg0@gmail.com>
2023-05-18 13:56:48 +0500

    chore: complete lesson 00 React Kabzda

--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,45 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<App />);
+root.render(<div>Hello</div>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+let a = {
+  name: 'Mike',
+  age: 10,
+  address: {
+    city: 'Ekb',
+    country: 'Russia',
+  },
+};
+
+let users = [
+  {
+    name: 'Mike',
+    age: 10,
+    address: {
+      city: 'Ekb',
+      country: 'Russia',
+    },
+  },
+  {
+    name: 'John',
+    age: 13,
+    address: {
+      city: 'Ekb',
+      country: 'Russia',
+    },
+  },
+];
+
+let b: Array<string | number | boolean>;
+b = [1, 2, 3, 4, 5];
+b = [];
+b = [1, 2, 'n', '3', '3'];
+b = [true, false];
+
+console.log(users[0].address.country);
