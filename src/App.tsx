3c0a3476bc61acf8a57745d0dfa5bc663de874d0
a-andrievskikh <sn0abg0@gmail.com>
2023-05-18 18:29:48 +0500

    chore: complete lesson 02 React Kabzda

--- src/App.tsx
+++ src/App.tsx
@@ -2,40 +2,62 @@ import React from 'react';
 import './App.css';
 
 export function App() {
+  console.log('App Rendering!');
   return (
-    <div>
-      This is APP component
+    <div className='width margin'>
+      <AppTitle />
       <Rating />
       <Accordion />
+      <Rating />
     </div>
   );
+}
+
+function AppTitle() {
+  console.log('AppTitle Rendering!');
+  return <>This is APP component </>;
 }
 
 function Rating() {
+  console.log('Rating Rendering!');
   return (
-    <div>
+    <>
       <Star />
       <Star />
       <Star />
       <Star />
       <Star />
-    </div>
-  );
-}
-
-function Accordion() {
-  return (
-    <div>
-      <h3>Menu</h3>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
-    </div>
+    </>
   );
 }
 
 function Star() {
+  console.log('Star Rendering!');
   return <div>Star</div>;
+}
+
+function Accordion() {
+  console.log('Accordion Rendering!');
+  return (
+    <div>
+      <AccordionTitle />
+      <AccordionBody />
+    </div>
+  );
+}
+
+function AccordionTitle() {
+  console.log('AccordionTitle Rendering!');
+  return <h3>Menu</h3>;
+}
+
+function AccordionBody() {
+  console.log('AccordionBody Rendering!');
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  );
 }
