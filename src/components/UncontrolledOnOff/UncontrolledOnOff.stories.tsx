69e192bf9d3d952ef81eb45e4f014b4be666b455
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-07-02 18:45:02 +0500

    chore: complete lesson 11 React Kabzda Storybook, defaultValue, repeating

---
+++ src/components/UncontrolledOnOff/UncontrolledOnOff.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { UncontrolledOnOff } from './UncontrolledOnOff'
+import { action } from '@storybook/addon-actions'
+
+export default {
+  title: 'UncontrolledOnOff Stories',
+  component: UncontrolledOnOff,
+}
+
+const callBack = action('on or off clicked!')
+
+export const OnMode = () => <UncontrolledOnOff defaultOn={true} onChange={callBack} />
+export const OffMode = () => <UncontrolledOnOff defaultOn={false} onChange={callBack} />
+
+export const DefaultInputValue = () => <input defaultValue={'hi'} />
--- src/components/UncontrolledOnOff/UncontrolledOnOff.tsx
+++ src/components/UncontrolledOnOff/UncontrolledOnOff.tsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react'
 
 type PropsType = {
   onChange: (on: boolean) => void
+  defaultOn?: boolean
 }
 
 export const UncontrolledOnOff = (props: PropsType) => {
 
-  const [on, setOn] = useState<boolean>(true)
+  const [on, setOn] = useState<boolean>(props.defaultOn ? props.defaultOn : false)
 
   const buttonStyle = {
     padding: '3px',
